36b90d4404340e65f86c9019e5b449406bd2df02
ketizh <145917939+keti7@users.noreply.github.com>
2023-11-09 17:28:04 +0400

    add javascript file

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Hello, World!");
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+  };
+  console.log(person.firstName);
+  
+const sum = 5 + 3;
+const difference = 10 - 4;
+const product = 6 * 7;
+const quotient = 20 / 4; 
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
+
+
+let x = 10;
+x += 3;
+x -= 5; 
+x *= 2; 
+x /= 4; 
+console.log(x);
+
+let str1 = "Hello";
+let str2 = " World";
+let combined = str1 + str2;
+
+console.log("The combined is: " + combined);
+
+
+
+let length = 7;
+let width = 5;
+let area = length * width;
+
+console.log("The area of the rectangle is: " + area);
+
+
+  
